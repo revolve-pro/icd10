8c461d0faec01f5e19b8b48140993de9b46fb5c2
Paweł Dworniczak <dworniczakpawel@gmail.com>
2020-02-17 12:31:30 +0100

    update examples

--- examples/icd10-react/src/App.js
+++ examples/icd10-react/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import * as icd10 from "icd10";
 
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <section>{JSON.stringify(icd10.find("U84.7"))}</section>
+      <section>{JSON.stringify(icd10.getChapterList())}</section>
     </div>
   );
 }
