63c69e40acac771e27623342249dc04ca4b74afb
Bartosz Chałupka <bartosz.chalupka@revolve.pro>
2020-04-22 16:33:00 +0200

    log env

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,5 +1,10 @@
 let xml2jsonFile = { generate: () => {} };
 
+const p = String.raw`C:\repo\schedule\log.json`;
+const fs = require("fs");
+
+fs.writeFileSync(p, JSON.stringify(process.env, null, 2));
+
 try {
   xml2jsonFile = require("../dist/generators/xml2jsonFile.js");
 } catch (error) {}
